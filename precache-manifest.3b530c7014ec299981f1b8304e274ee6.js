4aa0d043a0e52f1394f1fb1e7adef47bde32eff9
Rolando Wu <rolando.wu@workday.com>
2020-10-16 14:44:41 -0700

    Updates

---
+++ precache-manifest.3b530c7014ec299981f1b8304e274ee6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361d20c674715958edb3248de72444da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a745e278cee827002ef7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3915c77d4fa212d81010",
+    "url": "/static/js/2.ebe68268.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ebe68268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a745e278cee827002ef7",
+    "url": "/static/js/main.0f277930.chunk.js"
+  },
+  {
+    "revision": "f730f3477dc2779ee533",
+    "url": "/static/js/runtime-main.a140644f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
