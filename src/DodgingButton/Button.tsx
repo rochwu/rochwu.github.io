aa89de2d1bc53d6d389b60a398ad8a30bc11b4fd
Rolando Wu <rolando@Rolobook-Pro.local>
2022-02-21 17:06:23 -0800

    Restyled button and unfucked scheduled interface

--- src/DodgingButton/Button.tsx
+++ src/DodgingButton/Button.tsx
@@ -2,18 +2,16 @@ import styled, {CSSObject} from '@emotion/styled';
 
 import {BUTTON} from '../constants';
 
-const border = `${BUTTON.BORDER_SIZE}px groove ${BUTTON.COLOR}`;
-
 const agentOverride: CSSObject = {
   backgroundColor: 'transparent',
   margin: 0,
   padding: '4px 20px', // Keep it even for lost text calculation
   borderRadius: '32px',
   boxSizing: 'border-box', // clearer dimension calculations
-  border,
+  border: `${BUTTON.BORDER_SIZE}px outset ${BUTTON.COLOR}`,
   ':focus': {
-    outline: border,
-    outlineOffset: '2px',
+    outline: `3px double ${BUTTON.COLOR}`,
+    outlineOffset: '1px',
   },
 };
 
