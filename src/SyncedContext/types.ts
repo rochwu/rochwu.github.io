6a2c29c64ddccbacfcf80425923d773d1381481c
Rolando Wu <rochwu@users.noreply.github.com>
2022-03-16 06:55:28 -0700

    Messed up id

--- src/SyncedContext/types.ts
+++ src/SyncedContext/types.ts
@@ -2,6 +2,6 @@ import {MutableRefObject} from 'react';
 
 export type AnyFunction = ((...params: any[]) => any) | (() => any);
 
-export type ImmediateOptions = {id?: string};
+export type ImmediateOptions = {id?: string | number};
 
 export type CallbackRef<C extends AnyFunction> = MutableRefObject<C>;
